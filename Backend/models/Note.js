9b900b0f2e6af673d50dfee1e4a4bfa838b5d0b4
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2022-02-10 23:51:37 +0530

    Setup the routes

---
+++ Backend/models/Note.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const NotesSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+  });
+
+module.exports = mongoose.model('notes', NotesSchema)
